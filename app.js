81c1f76b4b1ba8f052893eef6917bc33e2800c98
Sean McWatters <mcwatterssean@gmail.com>
2022-01-01 20:46:34 -0500

    init

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Sean";
+let lastName = "McWatters";
+var age = 25;
 // Exercise 2
-
+let fullName = firstName + " " + lastName;
+let templateFullName = `${firstName} ${lastName}`;
+console.log("fullName: ", fullName);
+console.log("templateFullName:", templateFullName);
 // Exercise 3
+let myStory;
+let city = "York";
+let pasttime = "coding, working out, and gaming"
+
+myStory = `My name is ${fullName}. I live in ${city}, and enjoy ${pasttime}`;
+console.log(myStory);
